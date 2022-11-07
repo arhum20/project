b78840e6f6ca80acff52af40dba86038be87c330
arhum20 <arhum_khan@cate.org>
2022-11-07 10:05:54 -0800

    committing

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(1500, 1000);
+    img = loadImage('desktop/maze.png')
+  }
+  
+  function draw() {
+    background(200,100,100);
+    Image(img,0,0);
+   
+
+  }
+
